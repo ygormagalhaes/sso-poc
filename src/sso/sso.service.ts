2006e9c16db1e474e99058fbf9cbdd0feca3d199
Ygor <emaildoygor@gmail.com>
2019-04-30 19:07:53 -0300

    WIP

--- src/sso/sso.service.ts
+++ src/sso/sso.service.ts
@@ -7,13 +7,13 @@ export class SsoService {
     private readonly users: Map<number, UserDTO> = new Map();
 
     add(user: UserDTO) {
-        const key = new Date().getUTCMilliseconds();
+        const key = new Date().getTime()
         this.users.set(key, user);
         return key;
     }
 
     get(key: number): UserDTO {
-        const user = this.users.get(key);
+        const user = this.users.get(Number.parseInt(`${key}`));
         if (!user) {
             throw new Error('Usuário não está em cache.');
         }
